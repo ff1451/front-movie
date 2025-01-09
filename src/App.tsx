e5201e415958d65ed11d8bbc783a87b01ca8a495
ff1451 <ff1451@gmail.com>
2025-01-09 16:35:07 +0900

    zustand 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import Header from "./components/Header/Header";
 import MoviesSection from "./components/MoviesSection/index";
-import { AppProvider } from "./hook/useAppState";
 
 function App() {
   return (
-    <AppProvider>
+    <>
       <Header />
       <MoviesSection />
-    </AppProvider>
+    </>
   );
 }
 
